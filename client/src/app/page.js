d813fb0cc22a3177c4eb4a5b55b35cc149af2227
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-09 23:13:42 +0200

    New components

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,14 +1,18 @@
 import Image from "next/image";
 import Start from "@/components/Buttons/Start";
+import PopUp from "@/components/Sections/Popup";
+import Levels from "@/components/Sections/Levels";
 
 export default function Home() {
   return (
     <main>
       <div className="relative flex h-screen flex-col items-center justify-center bg-amber-800">
         <div className="flex flex-col items-center justify-center px-5 py-5 lg-px:20 lg:py-10">
-          <p className="text-5xl font-bold text-white [text-shadow:_2px_2px_2px_rgb(0_0_0_/_20%)] lg:text-7xl py-10">The Sandwalkers</p>
+          <p className="text-4xl text-center font-bold text-white [text-shadow:_2px_2px_2px_rgb(0_0_0_/_20%)] lg:text-7xl py-10">The Sandwalkers</p>
         </div>
         <Start page={1} />
+        <PopUp/>
+        <Levels/>
       </div>
     </main>
   );
---
+++ client/src/components/Sections/Levels.jsx
@@ -0,0 +1,10 @@
+"use client";
+import Image from "next/image";
+
+export default function Levels() {
+  return (
+    <div className="relative flex h-screen flex-row  flex-wrap items-center justify-center gap-10 bg-about pt-5 lg:flex-nowrap">
+      hola
+    </div>
+  );
+}
