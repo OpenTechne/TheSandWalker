68427ea36183372b2b0706916e8586b06b467651
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-09 22:06:03 +0200

    Home page bg

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,15 +1,16 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
-  return ( 
+  return (
     <main>
-      <div className="relative flex h-screen flex-col items-center justify-center bg-home">
-        <div className="flex flex-col items-center justify-center rounded-3xl border-2 bg-white/30 px-5 py-5 shadow-md backdrop-blur-lg lg:px-20 lg:py-10">
-          <p className="text-5xl font-bold text-white [text-shadow:_2px_2px_2px_rgb(0_0_0_/_20%)] lg:text-9xl">OpenTechne</p>
-
-          <p className=" p-3 text-xl font-normal text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)] lg:pt-7 lg:text-4xl">Where technology becomes art</p>
+      <div className="relative flex h-screen flex-col items-center justify-center bg-amber-800">
+        <div className="flex flex-col items-center justify-center px-5 py-5 lg-px:20 lg:py-10">
+          <p className="text-5xl font-bold text-white [text-shadow:_2px_2px_2px_rgb(0_0_0_/_20%)] lg:text-7xl py-10">The Sandwalkers</p>
+          <button className="transition ease-in-out delay-150 hover:scale-110 duration-300 rounded-full border-4 hover:bg-white/90 hover:text-amber-800 hover:border-amber-800 border-white shadow-md bg-white/30 text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)] p-4 lg:text-2xl">
+            Start the game
+          </button>
         </div>
       </div>
     </main>
-  )
+  );
 }
