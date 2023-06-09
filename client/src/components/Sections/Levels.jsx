1a82182729f79211c7a469a21c02aba376e9ee90
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-10 01:04:51 +0200

    Home & Level pages modified

--- client/src/components/Sections/Levels.jsx
+++ client/src/components/Sections/Levels.jsx
@@ -3,8 +3,32 @@ import Image from "next/image";
 
 export default function Levels() {
   return (
-    <div className="relative flex h-screen flex-row  flex-wrap items-center justify-center gap-10 bg-about pt-5 lg:flex-nowrap">
-      hola
+    <div>
+      <div className="relative flex-column text-center flex-wrap items-center justify-center pt-5 lg:flex-nowrap bg-levels bg-cover">
+        <p>Introduction</p>
+        <span>This game consists of...</span>
+      </div>
+      <div className="bg-amber-600 relative flex-column text-center flex-wrap items-center justify-center">
+        <p>Levels</p>
+        <ul className="flex flex-row bg-slate-300 p-5 gap-3">
+          <li className="bg-white">
+            <p>Level 1</p>
+            <p>In this level...</p>
+          </li>
+          <li className="bg-white">
+            <p>Level 2</p>
+            <p>In this level...</p>
+          </li>
+          <li className="bg-white">
+            <p>Level 3</p>
+            <p>In this level...</p>
+          </li>
+          <li className="bg-white">
+            <p>Level 4</p>
+            <p>In this level...</p>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,11 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      backgroundImage: theme =>( {
+        'home': "url('/background.png')",
+        'levels': "url('/foto.png')",
+      }),
     },
   },
   plugins: [],
