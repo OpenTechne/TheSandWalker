756a5580a2684655a564ed3d959331bed45dcbd8
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-10 18:42:27 +0200

    Css modifications of home page

--- client/src/components/Sections/Loop.jsx
+++ client/src/components/Sections/Loop.jsx
@@ -4,48 +4,23 @@ import Image from "next/image";
 export default function Loop() {
   return (
     <div class="marquee">
-      <div className="animation flex flex-row gap-2">
-        <div class="flex flex-row justify-center items-center gap-2">
-          <div>
-            <span>Hackaton ETH Prague 2023</span>
-            <Image src="/eth.png" width={35} height={35} />
-          </div>
-          <div class="image">
-            
-          </div>
-        </div>
-        <div class="flex flex-row justify-center items-center gap-2">
-          <div>
-            <span>Hackaton ETH Prague 2023</span>
-          </div>
-          <div class="image">
-            <Image src="/eth.png" width={35} height={35} />
-          </div>
-        </div>
-        <div class="flex flex-row justify-center items-center gap-2">
-          <div>
-            <span>Hackaton ETH Prague 2023</span>
-          </div>
-          <div class="image">
-            <Image src="/eth.png" width={35} height={35} />
-          </div>
-        </div>
-        <div class="flex flex-row justify-center items-center gap-2">
-          <div>
-            <span>Hackaton ETH Prague 2023</span>
-          </div>
-          <div class="image">
-            <Image src="/eth.png" width={35} height={35} />
-          </div>
-        </div>
-        <div class="flex flex-row justify-center items-center gap-2">
-          <div>
-            <span>Hackaton ETH Prague 2023</span>
-          </div>
-          <div class="image">
-            <Image src="/eth.png" width={35} height={35} />
-          </div>
-        </div>
+      <div class="track flex flex-row items-center gap-5">
+        <span>Hackaton ETH Prague 2023</span>
+        <Image src="/eth.png" width={100} height={100}/>
+        <span>Hackaton ETH Prague 2023</span>
+        <Image src="/eth.png" width={100} height={100}/>
+        <span>Hackaton ETH Prague 2023</span>
+        <Image src="/eth.png" width={100} height={100}/>
+        <span>Hackaton ETH Prague 2023</span>
+        <Image src="/eth.png" width={100} height={100}/>
+        <span>Hackaton ETH Prague 2023</span>
+        <Image src="/eth.png" width={100} height={100}/>
+        <span>Hackaton ETH Prague 2023</span>
+        <Image src="/eth.png" width={100} height={100}/>
+        <span>Hackaton ETH Prague 2023</span>
+        <Image src="/eth.png" width={100} height={100}/>
+        <span>Hackaton ETH Prague 2023</span>
+        <Image src="/eth.png" width={100} height={100}/>
       </div>
     </div>
   );
