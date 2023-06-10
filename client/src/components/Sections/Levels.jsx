2ae180419c510d1753bfee5b429b025f19839667
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-10 14:44:08 +0200

    Levels updated

--- client/src/components/Sections/Levels.jsx
+++ client/src/components/Sections/Levels.jsx
@@ -5,38 +5,26 @@ import Start from "@/components/Buttons/Start";
 export default function Levels() {
   return (
     <div>
-      <div className="relative flex h-screen flex-col items-center justify-center bg-levels bg-cover">
-
-      <div className="relative h-screen flex-row  flex-wrap items-center justify-center gap-10 bg-about pt-5 lg:flex-nowrap">
-      <div className="m-3 flex max-w-2xl flex-col items-center justify-center rounded-3xl border-2 border-white bg-white/30 p-5 shadow-md backdrop-blur-lg  lg:p-10">
-        <h1 className="pb-3 text-2xl font-semibold text-white lg:text-3xl">INTRODUCTION</h1>
-        <h2 className="pb-3 text-center text-5xl font-black tracking-tight text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)] lg:text-7xl">INTRODUCTION</h2>
-        <p className="rounded-3xl border border-white bg-white/40 p-3 text-center leading-6 text-orange-800 shadow-md backdrop-blur-md lg:p-6 lg:text-lg lg:font-medium">
-          In this game you should be able to escape from the pyramids of the desert.
-          Let's see if you can crack the code!
-        </p>
-
-        
-      </div>
-      <div className="flex max-w-2xl flex-col items-center justify-center rounded-3xl border-2 border-white bg-white/30 p-8 shadow-md backdrop-blur-lg">
-        <h2 className="pb-3 text-5xl font-bold text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)]">GAME LEVELS</h2>
-        <p className="px-10 text-center font-medium text-orange-800">Levels from 1 to...</p>
-
-        <p className="rounded-3xl border border-white bg-white/40 p-3 text-center leading-6 text-orange-800 shadow-md backdrop-blur-md lg:p-6 lg:text-lg lg:font-medium">
-          LEVEL 1
-        </p>
-        <p className="rounded-3xl border border-white bg-white/40 p-3 text-center leading-6 text-orange-800 shadow-md backdrop-blur-md lg:p-6 lg:text-lg lg:font-medium">
-          LEVEL 2
-        </p>
-        <p className="rounded-3xl border border-white bg-white/40 p-3 text-center leading-6 text-orange-800 shadow-md backdrop-blur-md lg:p-6 lg:text-lg lg:font-medium">
-          LEVEL 3
-        </p>
-        
+      <div className="relative flex h-screen flex-col items-center gap-5 justify-center bg-levels bg-cover">
+        <div className=" flex flex-col gap-2 items-center justify-center rounded-full border-4 border-white w-3/4 bg-white/20 shadow-xl backdrop-blur-sm p-2 lg:p-5">
+          <h1 className="text-center text-xl font-black tracking-tight text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)] lg:text-4xl">INTRODUCTION</h1>
+          <p className="rounded-3xl text-center leading-6 text-orange-800 lg:text-md">
+            In this game you should be able to escape from the pyramids of the desert. Let's see if you can crack the code!
+          </p>
+        </div>
+        <div className="flex max-w-2xl flex-col items-center justify-center p-8">
+          <h2 className="pb-3 text-5xl font-bold text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)]">GAME LEVELS</h2>
+          <ul className="rounded-3xl flex flex-row gap-3">
+            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-orange-800 shadow-md backdrop-blur-md lg:p-5 lg:text-lg">
+              <Image src="/lvl.jpg" width={500} height={500} />
+              <p>LVL 1</p>
+              <p className="text-xs">In this game you should be able to escape from the pyramids of the desert. Let's see if you can crack the code!</p>
+            </li>
+            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-orange-800 shadow-md backdrop-blur-md lg:p-5 lg:text-lg">LEVEL 2</li>
+            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-orange-800 shadow-md backdrop-blur-md lg:p-5 lg:text-lg">LEVEL 3</li>
+          </ul>
         </div>
       </div>
-      
-      </div>
     </div>
-    
   );
 }
--- client/src/components/Sections/Loop.jsx
+++ client/src/components/Sections/Loop.jsx
@@ -4,10 +4,25 @@ import Image from "next/image";
 export default function Loop() {
   return (
     <div class="marquee">
-      <div class="track">
-        <div class="content">
-          Hackaton ETH Prague 2023 - Hackaton ETH Prague 2023 - Hackaton ETH Prague 2023 - Hackaton ETH Prague 2023
-        </div>
+      <div class="track flex flex-row gap-5">
+        <span class="content">
+          Hackaton ETH Prague 2023 -
+        </span>
+        <span class="content">
+          Hackaton ETH Prague 2023 -
+        </span>
+        <span class="content">
+          Hackaton ETH Prague 2023 -
+        </span>
+        <span class="content">
+          Hackaton ETH Prague 2023 -
+        </span>
+        <span class="content">
+          Hackaton ETH Prague 2023 -
+        </span>
+        <span class="content">
+          Hackaton ETH Prague 2023 -
+        </span>
       </div>
     </div>
   );
