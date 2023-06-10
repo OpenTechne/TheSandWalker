f90f613e56efcb5fd21f5399f9e6444f12b29aeb
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-10 11:16:53 +0200

    Marquee implemented

---
+++ client/src/components/Sections/Loop.jsx
@@ -0,0 +1,14 @@
+"use client";
+import Image from "next/image";
+
+export default function Loop() {
+  return (
+    <div class="marquee">
+      <div class="track">
+        <div class="content">
+          Hackaton ETH Prague 2023 - Hackaton ETH Prague 2023 - Hackaton ETH Prague 2023 - Hackaton ETH Prague 2023
+        </div>
+      </div>
+    </div>
+  );
+}
