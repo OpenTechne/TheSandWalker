f0621fa6bcd3cb91e034001584ac0e5ea1bb27d0
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-10 17:23:48 +0200

    Update marquee

--- client/src/components/Sections/Levels.jsx
+++ client/src/components/Sections/Levels.jsx
@@ -7,8 +7,8 @@ export default function Levels() {
     <div>
       <div className="relative flex h-screen flex-col items-center gap-5 justify-center bg-levels bg-cover">
         <div className=" flex flex-col gap-2 items-center justify-center rounded-full border-4 border-white w-3/4 bg-white/20 shadow-xl backdrop-blur-sm p-2 lg:p-5">
-          <h1 className="text-center text-xl font-black tracking-tight text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)] lg:text-4xl">INTRODUCTION</h1>
-          <p className="rounded-3xl text-center leading-6 text-orange-800 lg:text-md">
+          <h1 className="text-center text-l font-black tracking-tight text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)] lg:text-4xl">INTRODUCTION</h1>
+          <p className="rounded-3xl text-center leading-6 text-orange-800 lg:text-sm">
             Welcome to the distopic realm of "The SandWalker". In this heart-pounding video game experience, 
             you'll assume the role of a skilled Cairo hacker, venturing into the vast reaches of Starknet. 
             Each level is a smart contract that needs to be 'hacked'.  
@@ -17,22 +17,22 @@ export default function Levels() {
             It's 100% open source and all levels are contributions made by other players.
           </p>
         </div>
-        <div className="flex max-w-2xl flex-col items-center justify-center p-8">
+        <div className="flex flex-col items-center justify-center p-8">
           <h2 className="pb-3 text-5xl font-bold text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)]">GAME LEVELS</h2>
           <ul className="rounded-3xl flex flex-row gap-3">
-            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-white shadow-md backdrop-blur-md lg:p-5 lg:text-lg w-96">
+            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-white shadow-md backdrop-blur-md lg:p-5 lg:text-lg">
               <p className="text-3xl p-2">LVL 1</p>
-              <Image src="/eth.svg" width={500} height={500} />
+              <Image src="/eth.svg" width={200} height={200} />
               <p className="text-xs p-5">In this game you should be able to escape from the pyramids of the desert. Let's see if you can crack the code!</p>
             </li>
-            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-white shadow-md backdrop-blur-md lg:p-5 lg:text-lg w-96">
+            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-white shadow-md backdrop-blur-md lg:p-5 lg:text-lg">
             <p className="text-3xl p-2">LVL 2</p>
-              <Image src="/eth.svg" width={500} height={500} />
+              <Image src="/eth.svg" width={200} height={200} />
               <p className="text-xs p-5">In this game you should be able to escape from the pyramids of the desert. Let's see if you can crack the code!</p>
             </li>
-            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-white shadow-md backdrop-blur-md lg:p-5 lg:text-lg w-96">
+            <li className="rounded-3xl border-white bg-white/30 p-3 text-center text-white shadow-md backdrop-blur-md lg:p-5 lg:text-lg">
             <p className="text-3xl p-2">LVL 3</p>
-              <Image src="/eth.svg" width={500} height={500} />
+              <Image src="/eth.svg" width={200} height={200} />
               <p className="text-xs p-5">In this game you should be able to escape from the pyramids of the desert. Let's see if you can crack the code!</p>
             </li>
             </ul>
--- client/src/components/Sections/Loop.jsx
+++ client/src/components/Sections/Loop.jsx
@@ -4,25 +4,48 @@ import Image from "next/image";
 export default function Loop() {
   return (
     <div class="marquee">
-      <div class="track flex flex-row gap-5">
-        <span class="content">
-          Hackaton ETH Prague 2023 -
-        </span>
-        <span class="content">
-          Hackaton ETH Prague 2023 -
-        </span>
-        <span class="content">
-          Hackaton ETH Prague 2023 -
-        </span>
-        <span class="content">
-          Hackaton ETH Prague 2023 -
-        </span>
-        <span class="content">
-          Hackaton ETH Prague 2023 -
-        </span>
-        <span class="content">
-          Hackaton ETH Prague 2023 -
-        </span>
+      <div className="animation flex flex-row gap-2">
+        <div class="flex flex-row justify-center items-center gap-2">
+          <div>
+            <span>Hackaton ETH Prague 2023</span>
+            <Image src="/eth.png" width={35} height={35} />
+          </div>
+          <div class="image">
+            
+          </div>
+        </div>
+        <div class="flex flex-row justify-center items-center gap-2">
+          <div>
+            <span>Hackaton ETH Prague 2023</span>
+          </div>
+          <div class="image">
+            <Image src="/eth.png" width={35} height={35} />
+          </div>
+        </div>
+        <div class="flex flex-row justify-center items-center gap-2">
+          <div>
+            <span>Hackaton ETH Prague 2023</span>
+          </div>
+          <div class="image">
+            <Image src="/eth.png" width={35} height={35} />
+          </div>
+        </div>
+        <div class="flex flex-row justify-center items-center gap-2">
+          <div>
+            <span>Hackaton ETH Prague 2023</span>
+          </div>
+          <div class="image">
+            <Image src="/eth.png" width={35} height={35} />
+          </div>
+        </div>
+        <div class="flex flex-row justify-center items-center gap-2">
+          <div>
+            <span>Hackaton ETH Prague 2023</span>
+          </div>
+          <div class="image">
+            <Image src="/eth.png" width={35} height={35} />
+          </div>
+        </div>
       </div>
     </div>
   );
