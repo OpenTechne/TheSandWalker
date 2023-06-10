a35760714cede58bc116f128d88c403be14323e2
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-11 00:36:53 +0200

    Update TryLevel

--- client/src/components/Buttons/TryLevel.jsx
+++ client/src/components/Buttons/TryLevel.jsx
@@ -5,7 +5,7 @@ export default function TryLevel(props) {
   return (
     <div className="flex w-full justify-center">
       <button
-        className="transition ease-in-out delay-150 hover:scale-110 duration-300 rounded-full border-4 hover:bg-black border-white shadow-md bg-white/30 text-white [text-shadow:_2px_2px_2px_rgb(0_0_0_/_20%)] p-2 lg:text-md"
+        className="transition ease-in-out delay-150 hover:scale-110 duration-300 rounded-full border-4 hover:bg-amber-900 hover:text-amber-100 hover:border-amber-100 bg-amber-100 shadow-md text-amber-900 border-amber-900 [text-shadow:_2px_2px_2px_rgb(0_0_0_/_20%)] p-2 lg:text-md"
         onClick={() => {
           window.location.href = "/level1";
         }}
