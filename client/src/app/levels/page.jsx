4f84628bc8ac441a663fc0b8a776a9751596d923
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-10 20:26:18 +0200

    Unlock images

--- client/src/app/levels/page.jsx
+++ client/src/app/levels/page.jsx
@@ -6,7 +6,7 @@ import TryLevel from "@/components/Buttons/TryLevel";
 export default function LevelsPage() {
   return (
     <div>
-      <div className="relative flex h-screen flex-col items-center gap-5 justify-center bg-levels bg-cover">
+      <div className="relative flex h-screen flex-col items-center gap-5 justify-center bg-home bg-cover">
         <div className=" flex flex-col gap-2 items-center justify-center rounded-full border-4 border-white w-5/6 bg-white/20 shadow-xl backdrop-blur-sm p-2 lg:p-5">
           <h1 className="text-center text-l font-black tracking-tight text-white [text-shadow:_1px_1px_1px_rgb(0_0_0_/_20%)] lg:text-4xl">INTRODUCTION</h1>
           <p className="rounded-3xl text-center leading-6 text-orange-800 lg:text-sm">
