b145f329bba837c6eafe846958cfd2c69e8e9691
Marina Terentii <marina.terentii_97@hotmail.com>
2023-06-10 22:08:08 +0200

    update level pages

--- client/src/components/Buttons/TryLevel.jsx
+++ client/src/components/Buttons/TryLevel.jsx
@@ -5,7 +5,7 @@ export default function TryLevel(props) {
   return (
     <div className="flex w-full justify-center">
       <button
-        className="transition ease-in-out delay-150 hover:scale-110 duration-300 rounded-full border-4 hover:bg-black border-white shadow-md bg-white/30 text-white [text-shadow:_2px_2px_2px_rgb(0_0_0_/_20%)] p-4 lg:text-2xl"
+        className="transition ease-in-out delay-150 hover:scale-110 duration-300 rounded-full border-4 hover:bg-black border-white shadow-md bg-white/30 text-white [text-shadow:_2px_2px_2px_rgb(0_0_0_/_20%)] p-2 lg:text-md"
         onClick={() => {
           window.location.href = "/level1";
         }}
--- client/src/components/Sections/Loop.jsx
+++ client/src/components/Sections/Loop.jsx
@@ -5,21 +5,21 @@ export default function Loop() {
   return (
     <div class="marquee">
       <div class="track flex flex-row items-center gap-5">
-        <span>Hackaton ETH Prague 2023</span>
+        <span>Hackathon ETH Prague 2023</span>
         <Image src="/eth.png" width={100} height={100}/>
-        <span>Hackaton ETH Prague 2023</span>
+        <span>Hackathon ETH Prague 2023</span>
         <Image src="/eth.png" width={100} height={100}/>
-        <span>Hackaton ETH Prague 2023</span>
+        <span>Hackathon ETH Prague 2023</span>
         <Image src="/eth.png" width={100} height={100}/>
-        <span>Hackaton ETH Prague 2023</span>
+        <span>Hackathon ETH Prague 2023</span>
         <Image src="/eth.png" width={100} height={100}/>
-        <span>Hackaton ETH Prague 2023</span>
+        <span>Hackathon ETH Prague 2023</span>
         <Image src="/eth.png" width={100} height={100}/>
-        <span>Hackaton ETH Prague 2023</span>
+        <span>Hackathon ETH Prague 2023</span>
         <Image src="/eth.png" width={100} height={100}/>
-        <span>Hackaton ETH Prague 2023</span>
+        <span>Hackathon ETH Prague 2023</span>
         <Image src="/eth.png" width={100} height={100}/>
-        <span>Hackaton ETH Prague 2023</span>
+        <span>Hackathon ETH Prague 2023</span>
         <Image src="/eth.png" width={100} height={100}/>
       </div>
     </div>
